e2276611d73bd93701ddb784046da80244b7c260
Ibanga Ibanga <mails.ibanga@yahoo.com>
2022-01-27 22:52:40 +0100

    making comments more concise

--- Cyclistic.R
+++ Cyclistic.R
@@ -9,23 +9,25 @@ path <- "C:\\Users\\HP\\Documents\\Data Analytics Projects\\Cyclistic\\Raw Data"
 files <- list.files(path, full.names = TRUE, pattern = "*.csv")
 raw_df <- map_df(files, ~read_csv(.))
 
-#TRANSFOM DATA
-#Add columns for ride duration (mins), ride distance (m), day of week, month
+#TRANSFOMING THE DATA
+#add necessary columns (ride duration/distance, month of the year, day of the week)
 transformed_df <- raw_df %>%
-  mutate(ride_duration = as.numeric(difftime(ended_at, started_at, unit="mins"))) %>%
+  mutate(ride_duration = as.numeric(difftime(ended_at, started_at, unit="mins"))) %>% 
   mutate(ride_distance = distHaversine(cbind(start_lng, start_lat), cbind(end_lng, end_lat))) %>%
   mutate(day_of_week = weekdays(started_at)) %>%
   mutate(ride_month = month(started_at, label = TRUE))
 
-#CLEAN DATA
-#Remove rides not starting or ending at a station
+#CLEANING THE DATA
+#No duplicate entries found in pre-checks!
+#Remove rides not starting from a station
+#Remove rides not ending at a station
 #Remove rides with duration <= 0
 #Remove rides with distance <= 0
 cleaned_df <- transformed_df %>%
-  filter(!is.na(start_station_name)) %>%
-  filter(!is.na(end_station_name)) %>%
+  filter(!is.na(start_station_name)) %>% 
+  filter(!is.na(end_station_name)) %>% 
   filter(ride_duration > 0) %>%
-  filter(ride_distance > 0)
+  filter(ride_distance > 0) 
 
 #ANALYSE DATA
 #Membership type, month, day of week aggregated by average and max duration and distance
@@ -33,18 +35,6 @@ result_df <- cleaned_df %>%
   group_by(member_casual, ride_month, day_of_week) %>%
   summarise(number_of_rides = n(), avg_ride_duration = mean(ride_duration), avg_ride_distance = mean(ride_distance))
 
-View(result_df)
-str(result_df)
-
-result_df %>%
-  ggplot(mapping = aes(x = ride_month, y = avg_ride_distance, color = member_casual)) +
-    geom_vline()
-
-
-#EXPORT SUMMARY DATA INTO CSV
-write_csv(summary_df, "C:\\Users\\HP\\Documents\\Data Analytics Projects\\Cyclistic\\Analysis_Results.csv")
-
-
-
-
-
+#WRITE RESULT DATAFRAME TO CSV
+result_file <- paste(getwd(), "/analysis_results.csv", sep = "") #assign filename + set path to wd
+write_csv(result_df, result_file)
